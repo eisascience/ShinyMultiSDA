07192166598fc03509acc4b38c5c1bc9f1f01609
Eisa Mahyari <eisamahyari@gmail.com>
2023-02-03 18:23:18 -0800

    bugfix

--- inst/app/app.R
+++ inst/app/app.R
@@ -126,7 +126,7 @@ ui <- dashboardPage(skin="red",
                                   ),
                                   
                                   
-                                  )),
+                                  ))#,
                         
                         
                         
